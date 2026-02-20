c911129f213f078c4a21de87af4ac70d7ab1810f
emily Crandall Fleischman <zyxw59@gmail.com>
2026-02-20 13:38:42 -0500

    manual parsing of config code
    gives better errors than `parse_display`

--- src/config.rs
+++ src/config.rs
@@ -80,10 +80,10 @@ impl FromStr for Config {
     type Err = Error;
 
     fn from_str(s: &str) -> Result<Self, Self::Err> {
-        #[derive(Debug, Clone, Copy, parse_display::Display, parse_display::FromStr)]
-        #[display(style = "lowercase")]
+        #[derive(Debug, Clone, Copy, strum::EnumString)]
+        #[strum(serialize_all = "lowercase")]
         enum Section {
-            #[from_str(ignore)]
+            #[strum(disabled)]
             None,
             Item,
             Recipe,
@@ -94,38 +94,40 @@ impl FromStr for Config {
         for line in s.lines().map(str::trim).filter(|s| !s.is_empty()) {
             if let Some(line) = line.strip_prefix('!') {
                 let split = line.find('[');
-                section = split
-                    .map_or(line, |split| &line[..split])
-                    .trim()
-                    .parse()
-                    .whatever_context("invalid config section")?;
+                let section_name = split.map_or(line, |split| &line[..split]).trim();
+                section = section_name.parse().with_whatever_context(|_| {
+                    format!("invalid config section {section_name:?}")
+                })?;
                 if let Some(split) = split {
+                    let config = &line[split..];
                     match section {
                         Section::Item => {
                             this.item_default.merge_from(
-                                line[split..]
+                                config
                                     .parse::<ConfigWrapper<_>>()
-                                    .whatever_context("failed to parse default item config")?
+                                    .with_whatever_context(|_| {
+                                        format!("failed to parse default item config {config:?}")
+                                    })?
                                     .0,
                             );
                         }
                         Section::Recipe => {
                             this.recipe_default.merge_from(
-                                line[split..]
+                                config
                                     .parse::<ConfigWrapper<_>>()
-                                    .whatever_context("failed to parse default recipe config")?
+                                    .with_whatever_context(|_| {
+                                        format!("failed to parse default recipe config {config:?}")
+                                    })?
                                     .0,
                             );
                         }
                         Section::Edge => {
-                            let ConfigWrapper2(in_config, out_config) = line[split..]
-                                .parse::<ConfigWrapper2<_>>()
-                                .whatever_context("failed to parse default edge config")?;
-                            let out_config = out_config
-                                .into_option()
-                                .unwrap_or_else(|| in_config.clone());
-                            this.edge_default.0.merge_from(in_config.0);
-                            this.edge_default.1.merge_from(out_config.0);
+                            let ConfigWrapper2(in_config, out_config) =
+                                config.parse::<ConfigWrapper2<_>>().with_whatever_context(
+                                    |_| format!("failed to parse default edge config {config:?}"),
+                                )?;
+                            this.edge_default.0.merge_from(in_config);
+                            this.edge_default.1.merge_from(out_config);
                         }
                         Section::None => unreachable!(),
                     }
@@ -133,15 +135,17 @@ impl FromStr for Config {
             } else {
                 match section {
                     Section::Item => {
-                        let ClassConfig { class, config } = line
-                            .parse()
-                            .whatever_context("failed to parse item config")?;
+                        let ClassConfig { class, config } =
+                            line.parse().with_whatever_context(|_| {
+                                format!("failed to parse item config {line:?}")
+                            })?;
                         this.item.insert(class, this.item_default.merge(config.0));
                     }
                     Section::Recipe => {
-                        let ClassConfig { class, config } = line
-                            .parse()
-                            .whatever_context("failed to parse recipe config")?;
+                        let ClassConfig { class, config } =
+                            line.parse().with_whatever_context(|_| {
+                                format!("failed to parse recipe config {line:?}")
+                            })?;
                         this.recipe
                             .insert(class, this.recipe_default.merge(config.0));
                     }
@@ -153,17 +157,14 @@ impl FromStr for Config {
                             out_config,
                         } = line
                             .parse()
-                            .whatever_context("failed to edge recipe config")?;
-                        let out_config = out_config
-                            .into_option()
-                            .unwrap_or_else(|| in_config.clone());
+                            .with_whatever_context(|_| format!("failed to edge config {line:?}"))?;
                         let recipe_class = (!recipe_class.is_empty()).then_some(recipe_class);
                         let item_class = (!item_class.is_empty()).then_some(item_class);
                         this.edge.insert(
                             (recipe_class, item_class),
                             (
-                                this.edge_default.0.merge(in_config.0),
-                                this.edge_default.1.merge(out_config.0),
+                                this.edge_default.0.merge(in_config),
+                                this.edge_default.1.merge(out_config),
                             ),
                         );
                     }
@@ -248,50 +249,107 @@ where
 {
 }
 
-#[derive(Clone, Debug, parse_display::Display, parse_display::FromStr)]
-#[display("{class}{config}")]
+#[derive(Clone, Debug)]
 struct ClassConfig<T> {
-    #[from_str(regex = r"[^\[]*")]
     class: SmolStr,
     config: ConfigWrapper<T>,
 }
 
-#[derive(Clone, Debug, parse_display::Display, parse_display::FromStr)]
-#[display("{recipe_class}:{item_class}{in_config}{out_config}")]
-struct EdgeClassConfig<T> {
-    #[from_str(regex = r"[^:]*")]
-    recipe_class: SmolStr,
-    #[from_str(regex = r"[^\[]*")]
-    item_class: SmolStr,
-    #[from_str(regex = r"\[[^\]]*]")]
-    in_config: ConfigWrapper<T>,
-    out_config: OptionWrapper<ConfigWrapper<T>>,
-}
+impl<T: FromStr> FromStr for ClassConfig<T>
+where
+    T::Err: snafu::Error + 'static,
+{
+    type Err = Error;
 
-#[derive(Clone, Debug, parse_display::Display, parse_display::FromStr)]
-enum OptionWrapper<T> {
-    #[display("")]
-    None,
-    #[display("{0}")]
-    Some(T),
-}
-
-impl<T> OptionWrapper<T> {
-    fn into_option(self) -> Option<T> {
-        match self {
-            Self::None => None,
-            Self::Some(value) => Some(value),
-        }
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let split = s
+            .find('[')
+            .with_whatever_context(|| format!("missing '[' in class config {s:?}"))?;
+        let class = s[..split].trim();
+        let config = s[split..]
+            .parse()
+            .with_whatever_context(|_| format!("failed to parse config in class config {s:?}"))?;
+        Ok(Self {
+            class: class.into(),
+            config,
+        })
     }
 }
 
-#[derive(Clone, Debug, parse_display::Display, parse_display::FromStr)]
-#[display("[{0}]")]
+#[derive(Clone, Debug)]
+struct EdgeClassConfig<T> {
+    recipe_class: SmolStr,
+    item_class: SmolStr,
+    in_config: T,
+    out_config: T,
+}
+
+impl<T: FromStr> FromStr for EdgeClassConfig<T>
+where
+    T::Err: snafu::Error + 'static,
+{
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let colon = s
+            .find(':')
+            .with_whatever_context(|| format!("missing ':' in edge class config {s:?}"))?;
+        let in_split = s
+            .find('[')
+            .with_whatever_context(|| format!("missing '[' in edge class config {s:?}"))?;
+        let recipe_class = s[..colon].trim().into();
+        let item_class = s[colon + 1..in_split].trim().into();
+        let ConfigWrapper2(in_config, out_config) = s[in_split..].parse()?;
+        Ok(Self {
+            recipe_class,
+            item_class,
+            in_config,
+            out_config,
+        })
+    }
+}
+
+#[derive(Clone, Debug)]
 struct ConfigWrapper<T>(T);
 
-#[derive(Clone, Debug, parse_display::Display, parse_display::FromStr)]
-#[display("{0}{1}")]
-struct ConfigWrapper2<T>(ConfigWrapper<T>, OptionWrapper<ConfigWrapper<T>>);
+impl<T: FromStr> FromStr for ConfigWrapper<T>
+where
+    T::Err: snafu::Error + 'static,
+{
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        Ok(Self(
+            s.strip_prefix('[')
+                .with_whatever_context(|| format!("missing '[' in config {s:?}"))?
+                .strip_suffix(']')
+                .with_whatever_context(|| format!("missing ']' in config {s:?}"))?
+                .parse()
+                .whatever_context("failed to parse [] delimited config")?,
+        ))
+    }
+}
+
+#[derive(Clone, Debug)]
+struct ConfigWrapper2<T>(T, T);
+
+impl<T: FromStr> FromStr for ConfigWrapper2<T>
+where
+    T::Err: snafu::Error + 'static,
+{
+    type Err = Error;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let (first, second) = s
+            .match_indices('[')
+            .nth(1)
+            .map_or((s, s), |(split, _)| s.split_at(split));
+        Ok(Self(
+            first.parse::<ConfigWrapper<T>>()?.0,
+            second.parse::<ConfigWrapper<T>>()?.0,
+        ))
+    }
+}
 
 macro_rules! parse_config {
     (
@@ -324,7 +382,9 @@ macro_rules! parse_config {
             fn from_str(s: &str) -> Result<Self, Self::Err> {
                 let mut this = Self::default();
                 for arg in s.split_terminator(',') {
-                    let (field, value) = arg.split_once('=').whatever_context("missing `=` in field")?;
+                    let (field, value) = arg
+                        .split_once('=')
+                        .with_whatever_context(|| format!("missing `=` in field {arg:?}"))?;
                     match field.trim() {
                         $(
                             stringify!($field) => {
@@ -332,9 +392,12 @@ macro_rules! parse_config {
                                     value
                                         .trim()
                                         .parse()
-                                        .whatever_context(
-                                            concat!("failed to parse field ", stringify!($field)),
-                                        )?
+                                        .with_whatever_context(|_| {
+                                            format!(
+                                                concat!("failed to parse field ", stringify!($field), " {:?}"),
+                                                value.trim(),
+                                            )
+                                        })?
                                 );
                             }
                         )*
